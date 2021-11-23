4fad4e11220569d8c39fae464c56760b38c5b60e
genffy <genffyl@gmail.com>
2021-11-23 12:45:57 +0800

    feat: new version

--- src/components/layout/index.ts
+++
@@ -1,4 +0,0 @@
-import { withInstall } from '@/utils';
-import layout from './layout.vue';
-const Layout = withInstall(layout);
-export default Layout;
--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,106 +1,155 @@
-import type {
-  ProjectConfig,
-  HeaderSetting,
-  MenuSetting,
-  TransitionSetting,
-  MultiTabsSetting,
-} from '#/config';
-import type { BeforeMiniState } from '#/store';
+import { defineStore } from 'pinia'
 
-import { defineStore } from 'pinia';
-import { store } from '@/store/index';
+import { store } from '../index'
 
-import { ThemeEnum } from '@/enums/appEnum';
-import { APP_DARK_MODE_KEY_ } from '@/enums/cacheEnum';
-// import { resetRouter } from '@/router';
-import { deepMerge } from '@/utils';
+export type LayoutType = 'Classic' | 'LeftTop' | 'Top' | 'Test'
 
-interface AppState {
-  darkMode?: ThemeEnum;
-  // Page loading status
-  pageLoading: boolean;
-  // project config
-  projectConfig: ProjectConfig | null;
-  // When the window shrinks, remember some states, and restore these states when the window is restored
-  beforeMiniInfo: BeforeMiniState;
+export interface AppState {
+  collapsed: boolean
+  showTags: boolean
+  showLogo: boolean
+  showNavbar: boolean
+  fixedHeader: boolean
+  layout: LayoutType
+  showBreadcrumb: boolean
+  showHamburger: boolean
+  showScreenfull: boolean
+  showUserInfo: boolean
+  title: string
+  logoTitle: string
+  userInfo: string
+  greyMode: boolean
+  showBackTop: boolean
+  showMenuTab: boolean
+  requestTime: boolean
 }
-let timeId: TimeoutHandle;
+
 export const useAppStore = defineStore({
   id: 'app',
   state: (): AppState => ({
-    darkMode: undefined,
-    pageLoading: false,
-    projectConfig: null,
-    beforeMiniInfo: {},
+    collapsed: false, // 菜单栏是否栏缩收
+    showLogo: true, // 是否显示logo
+    showTags: true, // 是否显示标签栏
+    showNavbar: true, // 是否显示navbar
+    fixedHeader: true, // 是否固定header
+    layout: 'Classic', // layout布局
+    showBreadcrumb: true, // 是否显示面包屑
+    showHamburger: true, // 是否显示侧边栏缩收按钮
+    showScreenfull: true, // 是否全屏按钮
+    showUserInfo: true, // 是否显示用户头像
+    title: 'vue-element-plus-admin', // 标题
+    logoTitle: 'vue-ElPlus-admin', // logo标题
+    userInfo: 'userInfo', // 登录信息存储字段-建议每个项目换一个字段，避免与其他项目冲突
+    greyMode: false, // 是否开始灰色模式，用于特殊悼念日
+    showBackTop: true, // 是否显示回到顶部
+    showMenuTab: false, // 是否固定一级菜单
+    requestTime: false // 是否在接口调用时添加时间戳，避免IE缓存
   }),
   getters: {
-    getPageLoading(): boolean {
-      return this.pageLoading;
+    getCollapsed(): boolean {
+      return this.collapsed
     },
-    getDarkMode(): 'light' | 'dark' | string {
-      return this.darkMode || localStorage.getItem(APP_DARK_MODE_KEY_) || 'light';
+    getShowLogo(): boolean {
+      return this.showLogo
     },
-
-    getBeforeMiniInfo(): BeforeMiniState {
-      return this.beforeMiniInfo;
+    getShowTags(): boolean {
+      return this.showTags
     },
-
-    getProjectConfig(): ProjectConfig {
-      return this.projectConfig || ({} as ProjectConfig);
+    getShowNavbar(): boolean {
+      return this.showNavbar
     },
-
-    getHeaderSetting(): HeaderSetting {
-      return this.getProjectConfig.headerSetting;
+    getFixedHeader(): boolean {
+      return this.fixedHeader
     },
-    getMenuSetting(): MenuSetting {
-      return this.getProjectConfig.menuSetting;
+    getLayout(): LayoutType {
+      return this.layout
     },
-    getTransitionSetting(): TransitionSetting {
-      return this.getProjectConfig.transitionSetting;
+    getShowBreadcrumb(): boolean {
+      return this.showBreadcrumb
     },
-    getMultiTabsSetting(): MultiTabsSetting {
-      return this.getProjectConfig.multiTabsSetting;
+    getShowHamburger(): boolean {
+      return this.showHamburger
     },
+    getShowScreenfull(): boolean {
+      return this.showScreenfull
+    },
+    getShowUserInfo(): boolean {
+      return this.showUserInfo
+    },
+    getTitle(): string {
+      return this.title
+    },
+    getLogoTitle(): string {
+      return this.logoTitle
+    },
+    getUserInfo(): string {
+      return this.userInfo
+    },
+    getGreyMode(): boolean {
+      return this.greyMode
+    },
+    getShowBackTop(): boolean {
+      return this.showBackTop
+    },
+    getShowMenuTab(): boolean {
+      return this.showMenuTab
+    },
+    getRequestTime(): boolean {
+      return this.requestTime
+    }
   },
   actions: {
-    setPageLoading(loading: boolean): void {
-      this.pageLoading = loading;
+    setCollapsed(collapsed: boolean): void {
+      this.collapsed = collapsed
     },
+    setShowLogo(showLogo: boolean): void {
+      this.showLogo = showLogo
+    },
+    setShowTags(showTags: boolean): void {
+      this.showTags = showTags
+    },
+    setShowNavbar(showNavbar: boolean): void {
+      this.showNavbar = showNavbar
+    },
+    setFixedHeader(fixedHeader: boolean): void {
+      this.fixedHeader = fixedHeader
+    },
+    setLayout(layout: LayoutType): void {
+      this.layout = layout
+    },
+    setBreadcrumb(showBreadcrumb: boolean): void {
+      this.showBreadcrumb = showBreadcrumb
+    },
+    setHamburger(showHamburger: boolean): void {
+      this.showHamburger = showHamburger
+    },
+    setScreenfull(showScreenfull: boolean): void {
+      this.showScreenfull = showScreenfull
+    },
+    setUserInfo(showUserInfo: boolean): void {
+      this.showUserInfo = showUserInfo
+    },
+    setTitle(title: string): void {
+      this.title = title
+    },
+    setLogoTitle(logoTitle: string): void {
+      this.logoTitle = logoTitle
+    },
+    setGreyMode(greyMode: boolean): void {
+      this.greyMode = greyMode
+    },
+    setShowBackTop(showBackTop: boolean): void {
+      this.showBackTop = showBackTop
+    },
+    setShowMenuTab(showMenuTab: boolean): void {
+      this.showMenuTab = showMenuTab
+    },
+    setRequestTime(requestTime: boolean): void {
+      this.requestTime = requestTime
+    }
+  }
+})
 
-    setDarkMode(mode: ThemeEnum): void {
-      this.darkMode = mode;
-      localStorage.setItem(APP_DARK_MODE_KEY_, mode);
-    },
-
-    setBeforeMiniInfo(state: BeforeMiniState): void {
-      this.beforeMiniInfo = state;
-    },
-
-    setProjectConfig(config: DeepPartial<ProjectConfig>): void {
-      this.projectConfig = deepMerge(this.projectConfig || {}, config);
-      // Persistent.setLocal(PROJ_CFG_KEY, this.projectConfig);
-    },
-
-    async resetAllState() {
-      // resetRouter();
-      // Persistent.clearAll();
-    },
-    async setPageLoadingAction(loading: boolean): Promise<void> {
-      if (loading) {
-        clearTimeout(timeId);
-        // Prevent flicker
-        timeId = setTimeout(() => {
-          this.setPageLoading(loading);
-        }, 50);
-      } else {
-        this.setPageLoading(loading);
-        clearTimeout(timeId);
-      }
-    },
-  },
-});
-
-// Need to be used outside the setup
 export function useAppStoreWithOut() {
-  return useAppStore(store);
+  return useAppStore(store)
 }
