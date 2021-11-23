5d2c6201386e09fbceb2689fa422c99e06b2da4a
genffy <genffyl@gmail.com>
2021-11-23 12:52:46 +0800

    feat: add cd.json

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ function pathResolve(dir: string) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/dist-pro/',
+  base: '/vue-next/',
   plugins: [
     vue(),
     vueJsx(),
